1611c50054f114f3a0c4e330db99467df221ec4f
cocoy chang <cocoychang@gmail.com>
2024-06-20 17:56:22 +0800

    for updates in test runner and pom.xml

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -6,8 +6,6 @@ import org.junit.runner.RunWith;
 import utility.BaseDriver;
 
 
-
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features",
@@ -15,33 +13,9 @@ import utility.BaseDriver;
         plugin = {"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm"}
 )
 
-//extends AbstractTestNGCucumberTests
 public class TestRunner extends BaseDriver {
-
-//    @BeforeClass
-//    public  void setUp() throws InterruptedException {
-//          System.out.println("Before class");
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-//        driver.get("https://www.saucedemo.com/");
-//        Thread.sleep(3000);
-//        driver.manage().window().maximize();
-//    }
-
-//    @Override
-//    @DataProvider
-//    //@DataProvider (parallel = true) -- For parallel execution support (which is not going to work for our code)
-//    public Object[][] scenarios() {
-//        return super.scenarios();
-//    }
-
-
-
-//    @After
-//    public  void teardown()  {
-//          driver.quit();
-//        //TestRunner.teardown();
-//    }
 
 }
 //allure generate allure-results --clean -o allure-report
+
+
